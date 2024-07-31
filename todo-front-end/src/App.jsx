9187977c28b23dea83caccbdbd41ad91f57711e2
yashsaini7060 <yk18052000@gmail.com>
2024-07-31 12:35:09 +0530

    added redux configs

--- todo-front-end/src/App.jsx
+++ todo-front-end/src/App.jsx
@@ -1,6 +1,10 @@
-
+import { useEffect } from "react"
+import { toast } from "react-hot-toast"
 function App() {
-
+  useEffect(() => {
+    toast.error("hello")
+    console.log("toast not working")
+  })
   return (
     <>
       <h1 className="text-3xl font-bold underline">
--- todo-front-end/src/main.jsx
+++ todo-front-end/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App.jsx";
+import store from "./redux/store.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster/>
+    </BrowserRouter>
+  </Provider>
+);
