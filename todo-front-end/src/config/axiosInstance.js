185d3419b20aad7cb10a76bb96dd4258ab9e62f3
yashsaini7060 <yk18052000@gmail.com>
2024-08-01 13:36:50 +0530

    changed env variable

--- todo-front-end/src/config/axiosInstance.js
+++ todo-front-end/src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5041/api/v1" ;
-const BASE_URL =  "http://localhost:5041/api/v1" ;
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5041/api/v1" ;
+// const BASE_URL =  "http://localhost:5041/api/v1" ;
 
 
 const axiosInstance = axios.create();
